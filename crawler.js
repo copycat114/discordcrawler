c138c194fffbd37ee00b81d41e479bdba7d06a32
copycat114 <superman1762@gmail.com>
2017-05-08 15:51:32 -0400

    Initial commit

---
+++ crawler.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+client.on('message', message => {
+  if(message.channel.type == "text") {
+    if(message.author.id != client.user.id) {
+      if(message.guild.id.toString() != client.channels.get(config.channelid).guild.id) {
+        if(message.toString().indexOf("https://discord.gg/") >= 0) {
+          const destin = client.channels.get(config.channelid);
+	      destin.send("Invite Link Found: " + message.content.substring(message.toString().indexOf("https://discord.gg/"),message.toString().indexOf("https://discord.gg/") + 24));
+		  destin.sendMessage("", {embed: {
+            color: 13376797,
+	        author: {
+              name: message.author.username + " in " + message.guild.name,
+              icon_url: message.author.avatarURL
+            },
+	        description: message.content
+          }});
+		  console.log('Found invite link: ' + message.content.substring(message.toString().indexOf("https://discord.gg/"),message.toString().indexOf("https://discord.gg/") + 24))
+        };
+      };
+    };
+  };
+});
+
+client.on('ready', () => {
+  console.log('Invite Crawler logged in as: ' + client.user.username);
+});
+
+client.login(config.token);
