529866700636cdcb75f6be467afdfe0ad79498cd
copycat114 <superman1762@gmail.com>
2017-05-08 16:08:21 -0400

    Add files via upload

--- crawler.js
+++ crawler.js
@@ -8,9 +8,9 @@ client.on('message', message => {
   if(message.channel.type == "text") {
     if(message.author.id != client.user.id) {
       if(message.guild.id.toString() != client.channels.get(config.channelid).guild.id) {
-        if(message.toString().indexOf("https://discord.gg/") >= 0) {
+        if(message.toString().indexOf("discord.gg/") >= 0) {
           const destin = client.channels.get(config.channelid);
-	      destin.send("Invite Link Found: " + message.content.substring(message.toString().indexOf("https://discord.gg/"),message.toString().indexOf("https://discord.gg/") + 24));
+	      destin.send("Invite Link Found: " + message.content.substring(message.toString().indexOf("discord.gg/"),message.toString().indexOf("discord.gg/") + 16));
 		  destin.sendMessage("", {embed: {
             color: 13376797,
 	        author: {
@@ -19,7 +19,7 @@ client.on('message', message => {
             },
 	        description: message.content
           }});
-		  console.log('Found invite link: ' + message.content.substring(message.toString().indexOf("https://discord.gg/"),message.toString().indexOf("https://discord.gg/") + 24))
+		  console.log('Found invite link: ' + message.content.substring(message.toString().indexOf("discord.gg/"),message.toString().indexOf("discord.gg/") + 16));
         };
       };
     };
